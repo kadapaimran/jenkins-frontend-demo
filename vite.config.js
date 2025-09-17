86384bb82f16969aedf6e916f33d78ee624f4ae0
kadapaimran <kadapaimran99@hotmail.com>
2025-09-17 10:10:35 +0530

    Mess

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/jenkins-demo/',
-  plugins: [react()]
+  plugins: [react()],
+  base: '/jenkins-frontend-demo/', // important for GitHub Pages
 })
